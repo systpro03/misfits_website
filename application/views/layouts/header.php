5849733cd7141b68039bcba0c9290899d70baf13
systpro04 <systemprogrammer04.com>
2026-09-16 10:59:26 +0800

    Fix title typo in header to use singular 'MISFITS RIDER'

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -13,7 +13,7 @@ if ($base_path && strpos($current_path, $base_path) === 0) {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= isset($title) ? htmlspecialchars($title) : 'MISFITS RIDERS'; ?></title>
+    <title><?= isset($title) ? htmlspecialchars($title) : 'MISFITS RIDER'; ?></title>
     
 
     <meta name="description"
