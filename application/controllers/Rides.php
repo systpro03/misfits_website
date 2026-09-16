a5d590407cc382fa13509552a489bd2be320f556
systpro04 <systemprogrammer04.com>
2026-09-16 13:46:48 +0800

    Update CORS configuration in Rides controller to allow all origins

--- application/controllers/Rides.php
+++ application/controllers/Rides.php
@@ -7,7 +7,7 @@ class Rides extends Public_Controller {
 	{
 		parent::__construct();
 		$this->load->model('Ride_model');
-		header('Access-Control-Allow-Origin: http://localhost:3001');
+		header('Access-Control-Allow-Origin: *');
 		header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 		header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 		header('Access-Control-Allow-Credentials: true');
