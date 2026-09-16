124837c92dd2b9ca8a16d0c3ba49665f3e9d88b0
systpro04 <systemprogrammer04.com>
2026-09-16 14:01:20 +0800

    Add additional error logging for 404 and database connection issues

--- application/logs/log-2026-09-16.php
+++ application/logs/log-2026-09-16.php
@@ -6,3 +6,8 @@ ERROR - 2026-09-16 02:03:51 --> Severity: error --> Exception: Unable to connect
 ERROR - 2026-09-16 02:03:56 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo for sql210.infinityfree.com failed: No such host is known.  C:\laragon\www\misfits\system\database\drivers\mysqli\mysqli_driver.php 211
 ERROR - 2026-09-16 02:03:56 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo for sql210.infinityfree.com failed: No such host is known.  C:\laragon\www\misfits\system\database\drivers\mysqli\mysqli_driver.php 211
 ERROR - 2026-09-16 02:03:56 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\misfits\system\database\DB_driver.php 434
+ERROR - 2026-09-16 05:58:52 --> 404 Page Not Found: Upcoming_api/upcoming_api
+ERROR - 2026-09-16 05:58:57 --> 404 Page Not Found: Upcoming_api/upco
+ERROR - 2026-09-16 05:59:01 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo for sql210.infinityfree.com failed: No such host is known.  C:\laragon\www\misfits\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2026-09-16 05:59:01 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo for sql210.infinityfree.com failed: No such host is known.  C:\laragon\www\misfits\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2026-09-16 05:59:01 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\misfits\system\database\DB_driver.php 434
