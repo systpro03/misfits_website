28c0649c786e98cc47b1269bbb59a0db0b2e460c
systpro04 <systemprogrammer04.com>
2026-09-16 14:56:19 +0800

    Remove redundant Vary header from CORS configuration

--- application/hooks/Cors.php
+++ application/hooks/Cors.php
@@ -9,7 +9,6 @@ class Cors
         header("Access-Control-Allow-Origin: *");
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-        header('Vary: Origin');
 
         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
             http_response_code(204);
