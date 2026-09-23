08d12e75839f7a991d218208fe8d6f16e13e52d5
systpro04 <systemprogrammer04.com>
2026-09-23 10:07:18 +0800

    Update database.php to set the hostname for the default database connection

--- application/config/database.php
+++ application/config/database.php
@@ -14,7 +14,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
+	'hostname' => 'sql210.infinityfree.com',
 	'username' => 'if0_42879278',
 	'password' => 'Misfits2026',
 	'database' => 'if0_42879278_misfits_data',
