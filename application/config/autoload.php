ca98be3e1febe5662b9337e6407da772bdcd3414
systpro04 <systemprogrammer04.com>
2026-09-23 10:05:41 +0800

    Update autoload.php to correct helper name from 'misfits_helper' to 'misfits'

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $autoload['packages'] = array();
 $autoload['libraries'] = array('database', 'session', 'form_validation');
 $autoload['drivers'] = array();
-$autoload['helper'] = array('url', 'form', 'html', 'misfits_helper');
+$autoload['helper'] = array('url', 'form', 'html', 'misfits');
 $autoload['config'] = array();
 $autoload['language'] = array();
 $autoload['model'] = array();
